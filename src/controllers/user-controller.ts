761dc495e2cd3e2dc79ba3ce6e0b1ac1d71065e1
ipaullly <ipaully22@gmail.com>
2020-07-17 17:51:38 +0300

    complete authorization, user CRUD operations

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,26 +1,113 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import { getRepository } from "typeorm";
+import { Request, Response} from "express";
+import { validate } from "class-validator";
 
-export class UserController {
+import { User } from "../entity/User";
 
-    private userRepository = getRepository(User);
+class UserController {
+  static listAll = async (req: Request, res: Response) => {
+    // get users from the database
+    const userRepository = getRepository(User);
+    const users = await userRepository.find({
+      select: ["id", "username", "role"] // we don't want to include passwords in the response body
+    });
+    // send the users object
+    res.send(users);
+  };
+  static getOneById = async (req: Request, res: Response) => {
+    // Get the ID from the url
+    const id: any = req.params.id;
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+    // get the user from database
+    const userRepository = getRepository(User);
+    try {
+      const user = await userRepository.findOneOrFail(id, {
+        select: ["id", "username", "role"] //  we don't need the password in the response body
+      });
+    } catch (error) {
+      res.status(404).send("User not found oya!");
+    }
+  };
+  static newUser = async (req: Request, res: Response) => {
+    // get parameters from the body
+    let { username, password, role } = req.body;
+    let user = new User();
+    user.username = username;
+    user.password = password;
+    user.role = role;
+
+    // validate if the parameters are valid
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      res.status(400).send(errors);
+      return;
     }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
+    // hash the password, to securely store on the db
+    user.hashPassword();
+
+    //try to save. if it fails, the username is already in use
+    const userRepository = getRepository(User);
+    try {
+      await userRepository.save(user);
+    } catch (e) {
+      res.status(409).send("username already in use");
+      return;
     }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+    // if everything is ok send a 201 response
+    res.status(201).send("User created")
+
+  };
+  static editUser = async (req: Request, res: Response) => {
+    // Get id from req url
+    const id = req.params.id;
+    // get values from the body
+    const { username, role } = req.body;
+
+    // try to find user on database
+    const userRepository = getRepository(User);
+    let user: User;
+    try {
+      user = await userRepository.findOneOrFail(id);
+    } catch (error) {
+      // if not found, send a 404 response
+      res.status(404).send("User not found oink!");
+      return;
     }
 
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+    //validate the new values from req body
+    user.username = username;
+    user.role = role;
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      res.status(400).send(errors);
+      return;
     }
+    // try to save, will exit with an error
+    try {
+      await userRepository.save(user);
+    } catch (e) {
+      res.status(409).send("username already in use");
+      return;
+    }
+    // after all send a 204 (no content , but accepted) response
+    res.status(204).send();
+  };
+  static deleteUser = async (req: Request, res: Response) => {
+    // get id from url
+    const id = req.params.id;
+    const userRepository = getRepository(User);
+    let user: User;
+    try {
+      user = await userRepository.findOneOrFail(id)
+    } catch (e) {
+      res.status(404).send("User not found");
+      return;
+    }
+    userRepository.delete(id);
+    // after all send a 204 - no content but accepted response
+  };
+};
 
-}
+export default UserController;
---
+++ src/middlewares/check-jwt.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
+import config from "../config/config";
+
+export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+  //retrieve token from request header
+  const token = <string>req.headers["auth"];
+  let jwtPayload;
+  // try validating the token and get the data
+  try {
+    jwtPayload = <any>jwt.verify(token, config.jwtSecret);
+    res.locals.jwtPayload = jwtPayload;
+  } catch (error) {
+    //if token is not valid, respond with a 401
+    res.status(401).send();
+    return;
+  }
+
+  // token valid for 1 hour
+  // we set a new token with every request
+  const { userId, username } = jwtPayload;
+  const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
+    expiresIn: "1h"
+  });
+  res.setHeader("token", newToken);
+
+  // call the next middleware
+  next();
+};
